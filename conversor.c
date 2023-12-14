8ac95e566084907ed0bbdb561495e5ed4a0bb0d1
Daniel Servino da Rocha <142444338+DanielSRocha@users.noreply.github.com>
2023-12-14 22:33:59 +0000

    Nova função temperatura (substituir as funçoes (celcius) e (fahrenheit) e algumas mudanças no codigo em geral)

--- conversor.c
+++ conversor.c
@@ -1,27 +1,60 @@
 #include <stdio.h>
 
-float Fcelsius(float celsius) {
-    float calculo = (celsius * 1.8) + 32;
-    printf("%.2f", calculo);
-    return calculo;
-}
+float temperatura(int opção1, float celcius, float fahrenheit) {
+    if(opção1 == 1) {
+        float resultado1 = (celcius * 1.8) + 32;
 
-float Ffahrenheit(float fahrenheit) {
-    float calculo = (fahrenheit - 32) / 1.8;
-    printf("%.2f", calculo);
-    return calculo;
+        printf("O valor de %.2fC em F é:%.2f\n\n\n\n", celcius, resultado1); 
+    } else if(opção1 == 2) {
+        float resultado2 = (fahrenheit - 32) / 1.8;
+
+        printf("O valor de %.2fF em C é:%.2f\n\n\n\n", fahrenheit, resultado2);
+    }
 }
 
 int main(void) {
-    float celsius = 5;
-    float fahrenheit = 50;
+
     int opção = 1;
+    float celcius, fahrenheit;
 
-    if(opção == 1) {
-        float resultado = Fcelsius(celsius);
-    } else if(opção == 2) {
-        float resultado = Ffahrenheit(fahrenheit);
+    while(opção != 0) {
+        printf("*****CONVERSOR DE UNIDADES*****\n\n");
+
+        printf("[1] Temperaturas\n");
+
+        printf("opção: ");
+        scanf("%d", &opção);
+
+        switch (opção) {
+            case 0:
+                printf("\n\nObrigado Por utulizar o Conversor de Unidades!!");
+                break;
+
+            case 1:
+                int opção1;
+
+                printf("\n[1] Celcius para Fahrenheit\n");
+                printf("[2] Fahrenheit para Celcius");
+
+                printf("\nopção: ");
+                scanf("%d", &opção1);
+                
+                if(opção1 == 1) {
+
+                    printf("\nDigite o valor em Celcius: ");
+                    scanf("%f", &celcius);
+
+                    celcius = temperatura(opção1, celcius, fahrenheit);
+                
+                } else if(opção1 == 2){
+
+                    printf("Digite o valor em Fahrenheit: ");
+                    scanf("%f", &fahrenheit);
+
+                    fahrenheit = temperatura(opção1, celcius, fahrenheit);
+                } else {
+                    printf("Opção invalida!!\n\n\n\n");
+                }
+        }
     }
-
-    return 0;
 }
